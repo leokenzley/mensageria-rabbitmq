ad1a63adf7cff947864df2e4f13b4f64f58fedf9
Leo Kenzley <leokenzley@gmail.com>
2024-01-28 07:30:41 -0300

    Criando o producer em java+springboot

--- producer-j/src/main/java/com/leokenzley/mensageria/producerj/ProducerjApplication.java
+++ producer-j/src/main/java/com/leokenzley/mensageria/producerj/ProducerjApplication.java
@@ -2,12 +2,29 @@ package com.leokenzley.mensageria.producerj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
 
 @SpringBootApplication
 public class ProducerjApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerjApplication.class, args);
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+		final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+		return rabbitTemplate;
+	}
+
+	@Bean
+	public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+		return new Jackson2JsonMessageConverter();
 	}
 
 }
--- producer-j/src/main/java/com/leokenzley/mensageria/producerj/service/dto/PrecoDTO.java
+++ producer-j/src/main/java/com/leokenzley/mensageria/producerj/service/dto/PrecoDTO.java
@@ -1,11 +1,14 @@
 package com.leokenzley.mensageria.producerj.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
 public class PrecoDTO implements Serializable {
+    @JsonProperty("codigoProduto")
     private String codigoProduto;
+    @JsonProperty("quantidadeProduto")
     private Integer quantidadeProduto;
 }
